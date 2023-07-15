c5e54bdab5ce878792ef73088baa9622f0529b94
unknown <giuliano.ursino.90@gmail.com>
2023-07-15 02:18:12 +0200

    third_iteration

--- messagemix.js
+++ messagemix.js
@@ -1,5 +1,7 @@
+alert('Welcome! Press OK if you are not OK!');
 const button = document.getElementById("click");
 const messagedisp = document.getElementById("message");
+let moto = document.getElementById('moto');
 const arrays = {
   timeArray: [
     "Tommorrow",
@@ -42,4 +44,14 @@ console.log(arrays.message());
 button.addEventListener("click", function () {
   const mixmsg = arrays.message();
   messagedisp.innerHTML = mixmsg;
-});
+  if(mixmsg.includes('motorcycle')===true){moto.style.display='block';vegetable.style.display='none';birthday.style.display='none';rock.style.display='none';pond.style.display='none'
+}else if(mixmsg.includes('vegetable')===true){moto.style.display='none';vegetable.style.display='block';birthday.style.display='none';rock.style.display='none';pond.style.display='none'
+}else if(mixmsg.includes('birthday')===true){moto.style.display='none';vegetable.style.display='none';birthday.style.display='block';rock.style.display='none';pond.style.display='none'
+}else if(mixmsg.includes('rock')===true){moto.style.display='none';vegetable.style.display='none';birthday.style.display='none';rock.style.display='block';pond.style.display='none'
+}else if(mixmsg.includes('pond')===true){moto.style.display='none';vegetable.style.display='none';birthday.style.display='none';rock.style.display='none';pond.style.display='block'
+}
+}
+);
+
+
+
