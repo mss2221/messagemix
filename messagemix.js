0aba8c863d34b81c81e5c8b180e3baed76d91b6d
MSaccomano <persischernachtgesang@gmail.com>
2023-07-15 14:15:43 +0200

    add ascii function, figlet library

--- messagemix.js
+++ messagemix.js
@@ -1,4 +1,7 @@
 
+const figlet = require("figlet")
+
+
 const arrays = {
   timeArray : ['Tommorrow', 'In ten minutes', 'Next year', 'Very soon', 'By October'],
   verbArray : ['you will discover', 'everything will be', 'your thoughts will turn to', 'you might be', 'you will receive'],
@@ -12,6 +15,62 @@ const arrays = {
     message:()=>{
         return `${arrays.timeArray[arrays.random()]} ${arrays.verbArray[arrays.random()]} ${arrays.adjectiveArray[arrays.random()]} ${arrays.nounArray[arrays.random()]} `
     }
+};
+
+
+words = arrays.message()
+/*
+
+This following code will split off the first word of the message
+and write an ASCII version of the first word to the console.
+
+However, the message is created before
+the makeAscii() function is finished.
+
+I want the ASCII word to appear first, then
+the rest of the message. (right now, the whole
+message is displayed. I will change it so the first
+word is left out.)
+
+The desired result:
+ _   _           _
+| \ | | _____  _| |_
+|  \| |/ _ \ \/ / __|
+| |\  |  __/>  <| |_
+|_| \_|\___/_/\_\\__|  year you might be a wonderful rock
+
+*/
+const makeAscii = (words) => {
+  wordsArray = words.split(' ');
+  firstWord = wordsArray[0];
+  figlet.text(firstWord, function (err, asciiWord) {
+   console.log(asciiWord);
+//   console.log(words);  -- this line within the function will ensure it appears after
+
+  })
+};
+
+makeAscii(words)
+console.log(words)
+
+/*
+I have started trying to make a Promise /async
+structure, but I cannot get it to work.
+
+function makeAscii() {
+  return new Promise(resolve => {
+    wordsArray = words.split(' ');
+    firstWord = wordsArray[0];
+    figlet.text(firstWord, function (err, asciiWord) {
+      console.log(asciiWord);
+    });
+  });
 }
 
-console.log(arrays.message())
+async function displayMessage() {
+  const msg = await makeAscii();
+  console.log(arrays.message);
+}
+
+displayMessage()
+*/
