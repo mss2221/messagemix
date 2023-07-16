faec4211a8fdb27b7375c8167797349c46426a26
MSaccomano <persischernachtgesang@gmail.com>
2023-07-16 21:28:49 +0200

    temp stage save

--- messagemixOrig.js
+++ messagemixOrig.js
@@ -24,25 +24,18 @@ const arrays = {
     "a wonderful",
     "a lovely",
   ],
-  nounArray: [
-  "motorcycle",
-  "vegetable",
-  "birthday party",
-  "rock",
-  "pond"
-  ],
+  nounArray: ["motorcycle", "vegetable", "birthday party", "rock", "pond"],
 
   random: () => {
     return Math.floor(Math.random() * 5);
   },
 
   message: () => {
-    newMessage = `${arrays.timeArray[arrays.random()]
-    } ${arrays.verbArray[arrays.random()]
-    } ${arrays.adjectiveArray[arrays.random()]
-    } ${arrays.nounArray[arrays.random()]
-    }`;
-    return newMessage;
+    return `${arrays.timeArray[arrays.random()]} ${
+      arrays.verbArray[arrays.random()]
+    } ${arrays.adjectiveArray[arrays.random()]} ${
+      arrays.nounArray[arrays.random()]
+    } `;
   },
 };
 
@@ -50,7 +43,6 @@ console.log(arrays.message());
 function dispImage(img) {
   document.getElementById(img).style.display = "block";
 }
-
 function hideImg() {
   moto.style.display = "none";
   vegetable.style.display = "none";
@@ -58,36 +50,17 @@ function hideImg() {
   rock.style.display = "none";
   pond.style.display = "none";
 }
-/*
-function assembleMessage () {
-  const mixmsg = arrays.message();
-  messagedisp.innerHTML = mixmsg;
-  if (mixmsg.includes("motorcycle") === true) {
-    hideImg();
-    dispImage("moto");
-  } else if (mixmsg.includes("vegetable") === true) {
-    hideImg();
-    dispImage("vegetable");
-  } else if (mixmsg.includes("birthday") === true) {
-    hideImg();
-    dispImage("birthday");
-  } else if (mixmsg.includes("rock") === true) {
-    hideImg();
-    dispImage("rock");
-  } else if (mixmsg.includes("pond") === true) {
-    hideImg();
-    dispImage("pond");
-  }
-}
-//button.addEventListener("click", assembleMessage());
-assembleMessage();
-*/
+
 button.addEventListener("click", function () {
   const mixmsg = arrays.message();
   messagedisp.innerHTML = mixmsg;
   if (mixmsg.includes("motorcycle") === true) {
     hideImg();
     dispImage("moto");
+    // vegetable.style.display = "none";
+    // birthday.style.display = "none";
+    // rock.style.display = "none";
+    // pond.style.display = "none";
   } else if (mixmsg.includes("vegetable") === true) {
     hideImg();
     dispImage("vegetable");
